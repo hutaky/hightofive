5b3326a654cc9057cf52de897214480933b3a9a3
madaXc <47150624+hutaky@users.noreply.github.com>
2025-11-01 17:29:13 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import DigBaseApp from "@/components/dig/DigBaseApp";
-import { FarcasterProvider } from "@/components/farcaster-provider";
+"use client";
+
+import type { Context } from '@farcaster/frame-sdk'
+import sdk from '@farcaster/frame-sdk'
+import { useQuery } from '@tanstack/react-query'
+import { type ReactNode, createContext, useContext } from 'react'
+
 
 export default function Home() {
   return (
