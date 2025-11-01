69cb81d18d80308ac3dffe5b682f2c59fd17c01d
madaXc <47150624+hutaky@users.noreply.github.com>
2025-11-01 18:47:05 +0100

    Rename Home function to Page and adjust imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-'use client'
-import DigBaseApp from '@/components/dig/DigBaseApp'
-import { Providers } from '@/components/providers'
+"use client";
 
-export default function Home() {
+import DigBaseApp from "@/components/dig/DigBaseApp";
+import { Providers } from "@/components/providers";
+
+export default function Page() {
   return (
     <Providers>
       <DigBaseApp />
     </Providers>
-  )
+  );
 }
