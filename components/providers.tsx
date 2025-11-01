01903732945ce416dca29692af4bfca213cd3e10
madaXc <47150624+hutaky@users.noreply.github.com>
2025-11-01 18:34:11 +0100

    Update providers.tsx

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,6 +1,18 @@
-'use client'
-import { FrameProvider } from '@/components/farcaster-provider'
+"use client";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <FrameProvider>{children}</FrameProvider>
+import { ReactNode } from "react";
+import { FrameProvider } from "@/components/farcaster-provider";
+import { WalletProvider } from "@/components/wallet-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <FrameProvider>
+        <WalletProvider>{children}</WalletProvider>
+      </FrameProvider>
+    </QueryClientProvider>
+  );
 }
